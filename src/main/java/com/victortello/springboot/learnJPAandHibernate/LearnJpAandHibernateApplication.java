c35a1d26f8f67f2540b1bd3d3e73f1a53b399a8a
Victor Hugo Tello <victorhugotello@hotmail.com>
2023-05-29 15:46:35 -0600

    first commit

---
+++ src/main/java/com/victortello/springboot/learnJPAandHibernate/LearnJpAandHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.victortello.springboot.learnJPAandHibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnJpAandHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnJpAandHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/victortello/springboot/learnJPAandHibernate/LearnJpAandHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victortello.springboot.learnJPAandHibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnJpAandHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

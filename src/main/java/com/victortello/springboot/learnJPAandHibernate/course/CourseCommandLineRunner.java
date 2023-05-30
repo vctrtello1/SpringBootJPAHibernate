5ee59d149c0d23c14c08ab1c0f21f686afb4cf6f
Victor Hugo Tello <victorhugotello@hotmail.com>
2023-05-29 19:27:37 -0600

    JPA Repository

--- src/main/java/com/victortello/springboot/learnJPAandHibernate/course/CourseCommandLineRunner.java
+++ src/main/java/com/victortello/springboot/learnJPAandHibernate/course/CourseCommandLineRunner.java
@@ -1,16 +1,22 @@
-package com.victortello.springboot.learnJPAandHibernate.course.jdbc;
+package com.victortello.springboot.learnJPAandHibernate.course;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import com.victortello.springboot.learnJPAandHibernate.course.Course;
+// import com.victortello.springboot.learnJPAandHibernate.course.jdbc.CourseJDBCRepository;
+import com.victortello.springboot.learnJPAandHibernate.course.jpa.CourseJPARepository;
 
 @Component
-public class CourseJDBCCommandLineRunner implements CommandLineRunner {
+public class CourseCommandLineRunner implements CommandLineRunner {
+
+    // switch to JPA Repository
+
+    // @Autowired
+    // private CourseJDBCRepository repository;
 
     @Autowired
-    private CourseJDBCRepository repository;
+    private CourseJPARepository repository;
 
     @Override
     public void run(String... args) throws Exception {
---
+++ src/main/java/com/victortello/springboot/learnJPAandHibernate/course/jpa/CourseJPARepository.java
@@ -0,0 +1,32 @@
+package com.victortello.springboot.learnJPAandHibernate.course.jpa;
+
+import org.springframework.stereotype.Repository;
+
+import com.victortello.springboot.learnJPAandHibernate.course.Course;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+
+@Repository
+@Transactional
+public class CourseJPARepository {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public void insert(Course course) {
+        entityManager.merge(course);
+
+    }
+
+    public Course findById(long id) {
+        return entityManager.find(Course.class, id);
+    }
+
+    public void deleteById(long id) {
+        Course course = entityManager.find(Course.class, id);
+        entityManager.remove(course);
+    }
+
+}
